b8ab928f053c7f2807af40ce05b79acaea45538d
sigqualityconsulting-a11y <sig.qualityconsulting@gmail.com>
2026-08-16 09:31:59 -0500

    Update config.js
    Change QR Domin

--- docs/config.js
+++ docs/config.js
@@ -3,7 +3,7 @@ window.ECOSPEAK_CONFIG = {
   APP_VERSION: "v3.23",
   BUILD_LABEL: "v3.23",
   EVENT_CODE: "VIVI-DEMO-001",
-  PUBLIC_APP_URL: "https://javieravellanedadiasorin-prog.github.io/comunica-con-impacto-vivi/",
+  PUBLIC_APP_URL: "https://sigqualityconsulting-a11y.github.io/comunica-con-impacto-vivi/",
 
   SUPABASE_URL: "https://bcixqnmsuypgyugwbipi.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_9zkf1bKDe8ldzYvYCqe8Ow_lm0F2ect",
